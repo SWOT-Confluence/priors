abba297f1bf5e5de28e450efef11655a374f5e13
Nikki Tebaldi <ntebaldi@umass.edu>
2021-11-09 07:50:36 -0500

    Implemented more robust GRDC test

--- tests/test_Sos.py
+++ tests/test_Sos.py
@@ -14,34 +14,24 @@ from priors.sos.Sos import Sos
 class test_SoS(unittest.TestCase):
     """Test SoS operations."""
 
-    SOS_FILE = Path(__file__).parent / "sos" / "na_sword_v11_SOS_priors.nc"
-    WORKING_FILE = Path(__file__).parent / "sos" / "working" / "na_sword_v11_SOS_priors.nc"
+    SOS_FILE1 = Path(__file__).parent / "sos" / "as_sword_v11_SOS_priors.nc"
+    GRDC_FILE = Path(__file__).parent / "sos" / "working1" / "as_sword_v11_SOS_priors.nc"
+    SOS_FILE2 = Path(__file__).parent / "sos" / "na_sword_v11_SOS_priors.nc"
+    USGS_FILE = Path(__file__).parent / "sos" / "working2" / "na_sword_v11_SOS_priors.nc"
 
-    def test_overwite_grades(self):
-        """Test overwrite_grades method."""
+    def test_overwite_grades_usgs(self):
+        """Test overwrite_grades method (USGS data)."""
 
-        if not self.WORKING_FILE.parent.exists(): self.WORKING_FILE.parent.mkdir(parents=True, exist_ok=True)
-        copyfile(self.SOS_FILE, self.WORKING_FILE)
+        if not self.USGS_FILE.parent.exists(): self.USGS_FILE.parent.mkdir(parents=True, exist_ok=True)
+        copyfile(self.SOS_FILE2, self.USGS_FILE)
 
-        sos = Sos("na", "constrained", self.SOS_FILE.parent)
-        sos.sos_file = self.WORKING_FILE
+        sos = Sos("na", "constrained", self.SOS_FILE2.parent)
+        sos.sos_file = self.USGS_FILE
         sos.overwrite_grades()
 
-        sos_ds = Dataset(self.WORKING_FILE, 'r')
+        sos_ds = Dataset(self.USGS_FILE, 'r')
         rids_sum = sos_ds["model"]["grdc"].dimensions["num_grdc_reaches"].size + sos_ds["model"]["usgs"].dimensions["num_usgs_reaches"].size
         self.assertEqual(rids_sum, sos_ds["model"].dimensions["num_overwritten"].size)
-        
-        # GRDC
-        expected_fdq = np.array([487.04895999999997, 393.60352, 356.79168, 328.47488, 300.15808, 267.59376, 227.667072, 180.43464959999991, 140.451328, 109.33116480000005, 85.516736, 69.37616, 59.182112, 51.536576, 45.30688, 39.077184, 33.98016, 30.582144, 27.27898928, 23.746468480000008])
-        assert_array_almost_equal(expected_fdq, sos_ds["model"]["flow_duration_q"][25902])
-        self.assertAlmostEqual(809.8604799999999, sos_ds["model"]["max_q"][25902])
-        expected_monthly = np.array([49.04572522580644, 40.21407428436578, 30.72144438709677, 30.467696933333336, 77.20958897587131, 236.66048768000002, 358.4176952124352, 349.62698153290324, 291.27491106133334, 195.7304227029831, 107.47680768888888, 66.08166778494623])
-        assert_array_almost_equal(expected_monthly, sos_ds["model"]["monthly_q"][25902])
-        self.assertAlmostEqual(155.62195988835538, sos_ds["model"]["mean_q"][25902])
-        self.assertAlmostEqual(17.6696832, sos_ds["model"]["min_q"][25902])
-        self.assertAlmostEqual(472.89056, sos_ds["model"]["two_year_return_q"][25902])
-
-        # USGS
         expected_fdq = np.array([917.8038330078125, 436.0790100097656, 314.3160095214844, 247.20599365234375, 203.31500244140625, 171.60000610351562, 146.11500549316406, 124.59400177001953, 106.73135803222642, 91.18000030517578, 78.72100067138672, 66.8280029296875, 56.63399887084961, 46.439998626708984, 37.6609992980957, 29.732999801635742, 22.709999084472656, 17.555999755859375, 13.309000015258789, 8.947999954223633])
         assert_array_almost_equal(expected_fdq, sos_ds["model"]["flow_duration_q"][10870])
         self.assertAlmostEqual(5691.6767578125, sos_ds["model"]["max_q"][10870])
@@ -52,5 +42,28 @@ class test_SoS(unittest.TestCase):
         self.assertAlmostEqual(1718.8299560546875, sos_ds["model"]["two_year_return_q"][10870])
 
         sos_ds.close()
+        rmtree(self.USGS_FILE.parent)
+        
+    def test_overwite_grades_grdc(self):
+        """Test overwrite_grades method (GRDC data)."""
 
-        rmtree(self.WORKING_FILE.parent)
+        if not self.GRDC_FILE.parent.exists(): self.GRDC_FILE.parent.mkdir(parents=True, exist_ok=True)
+        copyfile(self.SOS_FILE1, self.GRDC_FILE)
+
+        sos = Sos("as", "constrained", self.SOS_FILE1.parent)
+        sos.sos_file = self.GRDC_FILE
+        sos.overwrite_grades()
+
+        sos_ds = Dataset(self.GRDC_FILE, 'r')
+        self.assertEqual(sos_ds["model"]["grdc"].dimensions["num_grdc_reaches"].size, sos_ds["model"].dimensions["num_overwritten"].size)
+        expected_fdq = np.array([16686.0, 13622.0, 12153.0, 10697.599999999999, 9500.0, 8110.0, 6733.6, 5445.4, 4215.6, 3333.4, 2633.4, 2267.600000000001, 1983.0, 1738.3999999999996, 1603.4, 1360.0, 1220.0, 1082.2, 960.0, 821.0])
+        assert_array_almost_equal(expected_fdq, sos_ds["model"]["flow_duration_q"][74436])
+        self.assertAlmostEqual(21433.0, sos_ds["model"]["max_q"][74436])
+        expected_monthly = np.array([1128.7806451612903, 1079.6037735849056, 1515.3032258064516, 2206.648888888889, 3263.458064516129, 7822.517777777778, 12819.309677419355, 11074.606451612903, 8849.25111111111, 6103.058064516129, 2740.311111111111, 1582.0623655913978])
+        assert_array_almost_equal(expected_monthly, sos_ds["model"]["monthly_q"][74436])
+        self.assertAlmostEqual(5040.598466873517, sos_ds["model"]["mean_q"][74436])
+        self.assertAlmostEqual(445.0, sos_ds["model"]["min_q"][74436])
+        self.assertAlmostEqual(16750.0, sos_ds["model"]["two_year_return_q"][74436])
+
+        sos_ds.close()
+        rmtree(self.GRDC_FILE.parent)
